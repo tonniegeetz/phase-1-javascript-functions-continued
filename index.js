1c71f194163a1b8d5a8d5c8f4ad8d6093d889e50
AnthonyG-Dev <giterugicheru@gmail.com>
2024-04-04 15:54:48 +0300

    understand function expressions and returning functions

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+
+function saturdayFun(activity = 'roller-skate') {
+  return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun();
+
+let mondayWork = function (activity = 'go to the office') {
+  return `This Monday, I will ${activity}.`;
+};
+
+function wrapAdjective(outer = '*') {
+  return function (inner = 'special') {
+    return `You are ${outer}${inner}${outer}!`;
+  };
+}
